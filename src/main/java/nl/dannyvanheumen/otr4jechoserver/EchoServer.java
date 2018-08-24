aca9121ce00a8f5ce3cd54e77782b5500c7af358
Danny van Heumen <danny@dannyvanheumen.nl>
2018-08-24 22:44:15 +0200

    Blank project for otr4j echo server.

---
+++ src/main/java/nl/dannyvanheumen/otr4jechoserver/EchoServer.java
@@ -0,0 +1,10 @@
+package nl.dannyvanheumen.otr4jechoserver;
+
+import javax.annotation.Nonnull;
+
+public final class EchoServer {
+
+    public static void main(@Nonnull final String[] args) {
+        System.err.println("Hello world");
+    }
+}
